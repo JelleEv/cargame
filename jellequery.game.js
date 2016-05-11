bd2419ff654cc8826574adcee0cffe3626d1e094
Jelle <jelle_evens@hotmail.com>
2016-05-11 10:01:55 +0200

    update jellequery.game

--- jellequery.game.js
+++ jellequery.game.js
@@ -6,6 +6,12 @@
     var bulletHolder = new Array();
     var counter = 0;
     var shootCounter = 0;
+    var enemyHolder = new Array();
+    for (var i = 0; i < 100; i++) {
+        var att = new attacker1();//tmp
+        enemyHolder.push(att);
+    }
+
     _.enableKeyCapture();
     _.enableMouseTracking();
     window.requestAnimFrame = (function () {
@@ -20,7 +26,13 @@
         requestAnimFrame(animloop);
         update();
     })();
+
+
     function update() {
+        for (var i = 0; i < enemyHolder.length; i++) {
+            enemyHolder[i].update();//tmp
+            enemyHolder[i].render();//tmp
+        }
         counter++;
         shootCounter++;
         if (counter == Number.MAX_SAFE_INTEGER - 1)
@@ -40,9 +52,13 @@
         _.drawCircle(canvas.clientWidth / 2, canvas.clientHeight / 2, 1, "rgb(0,255,0)");
     }
     function plCar() {
-        this.xPos = 100;
-        this.yPos = 100;
+        this.xPos = 0;
+        this.yPos = 0;
         this.speed = 0;
+        this.xySpeed = {
+            x: 0,
+            y: 0
+        }
         this.angle = 0;
         this.ACCELERATION = 1.5;
         this.MAXSPEED = 14;
@@ -74,8 +90,10 @@
                     this.angle = 0;
                 else if (this.angle < 0)
                     this.angle = Math.PI * 2;
-                this.xPos += Math.cos(this.angle) * this.speed;
-                this.yPos += Math.sin(this.angle) * this.speed;
+                this.xySpeed.x = Math.cos(this.angle) * this.speed;
+                this.xySpeed.y = Math.sin(this.angle) * this.speed;
+                this.xPos += this.xySpeed.x;
+                this.yPos += this.xySpeed.y;
             }
 
         }
@@ -106,10 +124,9 @@
         }
     }
     function catchMouseEvents() {
-
         if (_.mouseDown) {
             if (shootCounter > 7) {
-                bulletHolder.push(new bul(_.giveRadsBetweenPoints(canvas.clientWidth / 2, canvas.clientHeight / 2, _.mouseCurrent.x - canvas.offsetLeft, _.mouseCurrent.y - canvas.offsetTop), pl.xPos, pl.yPos))
+                bulletHolder.push(new bul(_.giveRadsBetweenPoints(canvas.clientWidth / 2, canvas.clientHeight / 2, _.mouseCurrent.x - canvas.offsetLeft, _.mouseCurrent.y - canvas.offsetTop), canvas.clientWidth / 2, canvas.clientHeight / 2))
                 shootCounter = 0;
             }
         }
@@ -129,9 +146,44 @@
                 this.dead = true;
         }
         this.render = function () {
-            _.drawCircle(this.xPos, this.yPos, 3, "#0F0");
+            _.drawCircle(this.xPos, this.yPos, 1, "#0F0");
         }
-
+    }
+    function attacker1() {
+        this.pos = new startCor();
+        this.speed = 0;
+        this.angle;
+        this.MAXSPEED = 10 + Math.ceil(Math.random() * 5);
+        this.update = function () {
+            this.angle = _.giveRadsBetweenPoints(this.pos.x, this.pos.y, canvas.clientWidth / 2, canvas.clientHeight / 2);
+            //this.pos.x -= pl.xySpeed.x;
+            //this.pos.y -= pl.xySpeed.y;
+        }
+        this.render = function () {
+            _.drawCircle(this.xPos, this.yPos, 9, "#F00");
+        }
     }
 
+    function startCor() {
+        this.x = 0;
+        this.y = 0;
+        switch (Math.ceil(Math.random() * 4)) {
+            case 1://he starts up 
+                this.y = -100;
+                this.x = Math.ceil(Math.random() * canvas.clientWidth);
+                break;
+            case 2://he starts right
+                this.y = Math.ceil(Math.random() * canvas.clientHeight)
+                this.x = canvas.clientWidth + 100;
+                break;
+            case 3://he starts down
+                this.y = canvas.clientHeight + 100;
+                this.x = Math.ceil(Math.random() * canvas.clientWidth);
+                break;
+            case 4://he starts left
+                this.y = canvas.clientHeight - 100;
+                this.x = Math.ceil(Math.random() * canvas.clientWidth);
+                break;
+        }
+    }
 })();
